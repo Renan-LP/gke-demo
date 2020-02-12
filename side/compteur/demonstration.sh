17ef3dd31dc5998dbd9a2ebed978bde985d7e022
Renan-LP <renan.le-provost@cirruseo.com>
2020-02-12 15:06:26 +0100

    Ajout du script de démonstration Canary / Rolling / Blue-Green

--- side/compteur/demonstration.sh
+++ side/compteur/demonstration.sh
@@ -5,10 +5,95 @@
 echo "SELECTIONNEZ LE CHIFFRE CORRESPONDANT A VOTRE DEMONSTRATION"
 select i in CANARY ROLLING BLUE-GREEN; do
         if [ "$i" = "CANARY" ]; then
-                echo "Bonjour Canary"
-		cd Canary
+                echo "### Deployment Canary selectionné ###"
+		sleep 1
+		cd ./Canary
 		kubectl apply -f ./service-frontend.yml
-                break
+                echo "Création du service ..."
+		sleep 90
+		echo "Création terminée !"
+		sleep 1
+		SERVICE_IP=$(kubectl get service/front --output=json | jq -r '.status.loadBalancer.ingress[0].ip')
+		echo "l'adresse IP du service est : "
+		echo $SERVICE_IP
+		sleep 1
+		echo "Création du deployment site-bleu ..."
+		kubectl apply -f ./demo-deployment-bleu.yml
+		echo "Création terminée !"
+		sleep 1
+		echo "Création du deployment site-rouge ..."
+		kubectl apply -f ./demo-deployment-rouge.yml
+		echo "Création terminée !"
+		sleep 1
+		echo "services : "
+		kubectl get services
+		sleep 1
+		echo "deployments : "
+		kubectl get deployments
+		curl http://$SERVICE_IP
+		sleep 1
+		curl http://$SERVICE_IP
+		sleep 1
+		curl http://$SERVICE_IP
+		sleep 1
+		curl http://$SERVICE_IP
+		sleep 1
+		curl http://$SERVICE_IP
+		sleep 1
+		curl http://$SERVICE_IP
+		sleep 1
+		curl http://$SERVICE_IP
+                sleep 1
+                curl http://$SERVICE_IP
+                sleep 1
+                curl http://$SERVICE_IP
+                sleep 1
+                curl http://$SERVICE_IP
+                sleep 1
+                curl http://$SERVICE_IP
+                sleep 1
+                curl http://$SERVICE_IP
+                sleep 1
+                curl http://$SERVICE_IP
+                sleep 1
+                curl http://$SERVICE_IP
+                sleep 1
+                curl http://$SERVICE_IP
+                sleep 1
+                curl http://$SERVICE_IP
+                sleep 1
+                curl http://$SERVICE_IP
+                sleep 1
+                curl http://$SERVICE_IP
+                sleep 1
+                curl http://$SERVICE_IP
+		sleep 1
+		curl http://$SERVICE_IP
+                sleep 1
+                curl http://$SERVICE_IP
+                sleep 1
+                curl http://$SERVICE_IP
+                sleep 1
+                curl http://$SERVICE_IP
+                sleep 1
+                curl http://$SERVICE_IP
+                sleep 1
+                curl http://$SERVICE_IP
+                sleep 1
+                curl http://$SERVICE_IP
+		echo "Suppression des deployments ..."
+		kubectl delete deployments site-rouge
+		kubectl delete deployments site-bleu
+		sleep 1
+		echo "Deployments supprimés !"
+		echo "Suppression du service ..."
+		kubectl delete service front
+		sleep 1
+		echo "Service supprimé !"
+		sleep 1
+		echo "Fin de la démonstration Canary !"
+		cd ..
+		break
         elif [ "$i" = "ROLLING" ]; then
                 echo "Bonjour Rolling"
                 break
@@ -16,7 +101,7 @@ select i in CANARY ROLLING BLUE-GREEN; do
                 echo "Bonjour Blue-Green"
                 break
         else
-                echo "mauvaise reponse"
+                echo "VOTRE REPONSE EST INCORRECTE, VEUILLEZ RELANCER LE PROGRAMME"
 		break
         fi
 done
